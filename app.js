2d69b1d0d99097748632b34991c9156607fbd9fc
Caleb Morse <calebmorse.code@gmail.com>
2022-11-28 16:48:15 -0600

    updated index, added DOM to app

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 // Rock paper scissor game
 
-// function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-
-// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-// Use prompt() to get input from the user.
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -16,9 +8,8 @@ const computerPlay = () => {
 	return choices[Math.floor(Math.random() * choices.length)];
 };
 
-const playRound = () => {
+const playRound = (playerSelection) => {
 	let computerSelection = computerPlay();
-	let playerSelection = prompt('Rock, paper, or scissors?');
 	let result = '';
 
 	if (
@@ -27,53 +18,50 @@ const playRound = () => {
 		(playerSelection == 'paper' && computerSelection == 'rock')
 	) {
 		playerScore += 1;
-		console.log(
-			(result =
-				'You win! ' +
-				playerSelection +
-				' beats ' +
-				computerSelection +
-				'<br><br>Player score: ' +
-				playerScore +
-				'<br>Computer score: ' +
-				computerScore)
-		);
+		result =
+			'You win! ' +
+			playerSelection +
+			' beats ' +
+			computerSelection +
+			'<br><br>Player score: ' +
+			playerScore +
+			'<br>Computer score: ' +
+			computerScore;
 
 		if (playerScore == 5) {
-			console.log(
-				(result += '<br><br>You won the game! Reload the page to play again')
-			);
+			result += '<br><br>You won the game! Reload the page to play again';
 		}
 	} else if (playerSelection == computerSelection) {
-		console.log(
-			(result =
-				"It's a tie. You both chose " +
-				playerSelection +
-				'<br><br>Player score: ' +
-				playerScore +
-				'<br>Computer score: ' +
-				computerScore)
-		);
+		result =
+			"It's a tie. You both chose " +
+			playerSelection +
+			'<br><br>Player score: ' +
+			playerScore +
+			'<br>Computer score: ' +
+			computerScore;
 	} else {
 		computerScore += 1;
-		console.log(
-			(result =
-				'You lose! ' +
-				computerSelection +
-				' beats ' +
-				playerSelection +
-				'<br><br>Player score: ' +
-				playerScore +
-				'<br>Computer score: ' +
-				computerScore)
-		);
+		result =
+			'You lose! ' +
+			computerSelection +
+			' beats ' +
+			playerSelection +
+			'<br><br>Player score: ' +
+			playerScore +
+			'<br>Computer score: ' +
+			computerScore;
 
 		if (computerScore == 5) {
-			console.log(
-				(result += '<br><br>I won the game! Reload the page to play again')
-			);
+			result += '<br><br>I won the game! Reload the page to play again';
 		}
 	}
-
+	document.getElementById('results').innerHTML = result;
 	return;
 };
+
+const btns = document.querySelectorAll('.btn');
+for (const btn of btns) {
+	btn.addEventListener('click', function () {
+		playRound(btn.value);
+	});
+}
